3b65d7df4deced2e024e2ae4c2045adefa42e925
pietropoerio91-droid <pietro.poerio91@gmail.com>
2025-09-20 10:51:33 +0200

    Delete HelpDeskAI-lc directory

--- HelpDeskAI-lc/src/main/java/com/example/chatbot/dto/ChatRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.chatbot.dto;
-
-public class ChatRequest {
-    private String message;
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
-}
